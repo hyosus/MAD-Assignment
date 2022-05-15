b021ecfe5f61ca74a1124196a615b8335e01082f
Shanice Yeong <shaniceyeong@gmail.com>
2022-05-16 00:37:22 +0800

    Homescreen if there are no trips added at all

--- app/src/main/java/sg/edu/np/mad/assignment/HomeActivity.java
+++ app/src/main/java/sg/edu/np/mad/assignment/HomeActivity.java
@@ -1,14 +1,33 @@
 package sg.edu.np.mad.assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Object Trips;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        if (Trips == null){
+            //Log.v("test","empty");
+            loadFragment(new NoTripsFragment());
+        }
+
+    }
+
+    public void loadFragment(Fragment fragment){
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragFrame, fragment);
+        ft.commit();
     }
 }
---
+++ app/src/main/java/sg/edu/np/mad/assignment/Trips.java
@@ -0,0 +1,11 @@
+package sg.edu.np.mad.assignment;
+
+import java.util.Date;
+
+public class Trips {
+    String destination;
+    Date startDate;
+    Date endDate;
+    String tripName;
+
+}
