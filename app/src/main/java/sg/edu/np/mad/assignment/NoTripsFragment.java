bc5048676a8764dd70ddd619798c15e88eaf2723
Shanice Yeong <shaniceyeong@gmail.com>
2022-05-15 23:00:08 +0800

    Added bottom nav bar

---
+++ app/src/main/java/sg/edu/np/mad/assignment/NoTripsFragment.java
@@ -0,0 +1,24 @@
+package sg.edu.np.mad.assignment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class NoTripsFragment extends Fragment {
+
+    public NoTripsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_no_trips, container, false);
+    }
+}
