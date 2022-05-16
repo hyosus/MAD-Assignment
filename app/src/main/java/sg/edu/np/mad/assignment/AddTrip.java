b9a74d1da36c79b7f28d5f6c902b049249e00519
Shanice Yeong <shaniceyeong@gmail.com>
2022-05-17 02:13:59 +0800

    Added date picker

--- app/src/main/java/sg/edu/np/mad/assignment/AddTrip.java
+++ app/src/main/java/sg/edu/np/mad/assignment/AddTrip.java
@@ -1,16 +1,36 @@
 package sg.edu.np.mad.assignment;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.media.Image;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class AddTrip extends AppCompatActivity {
     private ImageView back;
+    final Calendar startCalendar= Calendar.getInstance();
+    final Calendar endCalendar= Calendar.getInstance();
 
+    EditText sd,ed;
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +46,52 @@ public class AddTrip extends AppCompatActivity {
                 startActivity(Intent);
             }
         }));
+
+        // Get user input
+        ImageView save = findViewById(R.id.saveBtn);
+        TextView name = findViewById(R.id.nameTxt);
+        sd = findViewById(R.id.startTxt);
+        ed = findViewById(R.id.endTxt);
+        TextView dest = findViewById(R.id.destinationTxt);
+
+        save.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Trips t = new Trips(dest.toString(), sd.toString(), ed.toString(), name.toString());
+                Intent Intent = new Intent(AddTrip.this, HomeActivity.class);
+                startActivity(Intent);
+
+                Log.v("test", t.toString());
+            }
+        }));
+
+        //  Initiate DatePicker
+        DatePickerDialog.OnDateSetListener date = (view, year, month, day) -> {
+            startCalendar.set(Calendar.YEAR, year);
+            startCalendar.set(Calendar.MONTH,month);
+            startCalendar.set(Calendar.DAY_OF_MONTH,day);
+            updateLabel();
+        };
+        sd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddTrip.this, AlertDialog.THEME_HOLO_LIGHT, date,startCalendar.get(Calendar.YEAR),startCalendar.get(Calendar.MONTH),startCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        ed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(AddTrip.this, AlertDialog.THEME_HOLO_LIGHT, date,endCalendar.get(Calendar.YEAR),endCalendar.get(Calendar.MONTH),endCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+    }
+
+    private void updateLabel(){
+        String myFormat="MM/dd/yyyy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        sd.setText(dateFormat.format(startCalendar.getTime()));
+        ed.setText(dateFormat.format(endCalendar.getTime()));
     }
 }
--- app/src/main/java/sg/edu/np/mad/assignment/Trips.java
+++ app/src/main/java/sg/edu/np/mad/assignment/Trips.java
@@ -4,8 +4,14 @@ import java.util.Date;
 
 public class Trips {
     String destination;
-    Date startDate;
-    Date endDate;
+    String startDate;
+    String endDate;
     String tripName;
 
+    public Trips (String destination, String startDate, String endDate, String tripName) {
+        this.destination = destination;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.tripName = tripName;
+    }
 }
