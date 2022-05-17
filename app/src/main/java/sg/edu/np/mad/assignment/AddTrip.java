84b44ffd8b80116c0f511b7379fc26999003d9ce
Shanice Yeong <shaniceyeong@gmail.com>
2022-05-17 17:44:42 +0800

    User input validation

--- app/src/main/java/sg/edu/np/mad/assignment/AddTrip.java
+++ app/src/main/java/sg/edu/np/mad/assignment/AddTrip.java
@@ -24,11 +24,12 @@ import java.util.Date;
 import java.util.Locale;
 
 public class AddTrip extends AppCompatActivity {
-    private ImageView back;
+    private ImageView back, save;
+    private EditText name, dest, sd, ed;
+
+
     final Calendar startCalendar= Calendar.getInstance();
     final Calendar endCalendar= Calendar.getInstance();
-
-    EditText sd,ed;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -48,20 +49,20 @@ public class AddTrip extends AppCompatActivity {
         }));
 
         // Get user input
-        ImageView save = findViewById(R.id.saveBtn);
-        TextView name = findViewById(R.id.nameTxt);
+        save = findViewById(R.id.saveBtn);
+        name = findViewById(R.id.nameTxt);
         sd = findViewById(R.id.startTxt);
         ed = findViewById(R.id.endTxt);
-        TextView dest = findViewById(R.id.destinationTxt);
+        dest = findViewById(R.id.destinationTxt);
 
+        // Save user input
         save.setOnClickListener((new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Trips t = new Trips(dest.toString(), sd.toString(), ed.toString(), name.toString());
-                Intent Intent = new Intent(AddTrip.this, HomeActivity.class);
-                startActivity(Intent);
-
-                Log.v("test", t.toString());
+                validation();
+//                Trips t = new Trips(dest.toString(), sd.toString(), ed.toString(), name.toString());
+//                Intent Intent = new Intent(AddTrip.this, HomeActivity.class);
+//                startActivity(Intent);
             }
         }));
 
@@ -72,6 +73,15 @@ public class AddTrip extends AppCompatActivity {
             startCalendar.set(Calendar.DAY_OF_MONTH,day);
             updateLabel();
         };
+
+        DatePickerDialog.OnDateSetListener enddate = (view, year, month, day) -> {
+            endCalendar.set(Calendar.YEAR, year);
+            endCalendar.set(Calendar.MONTH,month);
+            endCalendar.set(Calendar.DAY_OF_MONTH,day);
+            updateLabel();
+        };
+
+        // Start date
         sd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -79,18 +89,50 @@ public class AddTrip extends AppCompatActivity {
             }
         });
 
+        // End date
         ed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DatePickerDialog(AddTrip.this, AlertDialog.THEME_HOLO_LIGHT, date,endCalendar.get(Calendar.YEAR),endCalendar.get(Calendar.MONTH),endCalendar.get(Calendar.DAY_OF_MONTH)).show();
+                new DatePickerDialog(AddTrip.this, AlertDialog.THEME_HOLO_LIGHT, enddate,endCalendar.get(Calendar.YEAR),endCalendar.get(Calendar.MONTH),endCalendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
     }
 
+    private void validation() {
+        String title = name.getText().toString();
+        String destination = dest.getText().toString();
+        String sDate = sd.getText().toString();
+        String eDate = ed.getText().toString();
+
+        if (title.isEmpty() ){
+            showError(name, "Missing information");
+        }
+        else if (destination.isEmpty()) {
+            showError(dest, "Missing information");
+        }
+        else if (sDate.isEmpty()){
+            showError(sd, "Missing information");
+        }
+        else if (eDate.isEmpty() || eDate.compareTo(sDate)<0){
+            showError(ed, "Missing information");
+            Log.v("testdate", "end date before start date");
+        }
+        else {
+            Log.v("testdate", sDate+eDate);
+            Trips t = new Trips(destination,sDate, eDate,title);
+            Intent Intent = new Intent(AddTrip.this, HomeActivity.class);
+            startActivity(Intent);
+        }
+    }
+
+    private void showError(EditText input, String missing_information) {
+        input.setError(missing_information);
+        input.requestFocus();
+    }
+
     private void updateLabel(){
-        String myFormat="MM/dd/yyyy";
-        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        SimpleDateFormat dateFormat=new SimpleDateFormat("dd/MM/yyy");
         sd.setText(dateFormat.format(startCalendar.getTime()));
         ed.setText(dateFormat.format(endCalendar.getTime()));
     }
