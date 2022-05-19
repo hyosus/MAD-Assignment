b382502f80f4a32da5ceddb119e371061904d159
Shanice Yeong <shaniceyeong@gmail.com>
2022-05-19 18:36:54 +0800

    refactor

--- app/src/main/java/sg/edu/np/mad/assignment/TripsFragment.java
+++ app/src/main/java/sg/edu/np/mad/assignment/TripsFragment.java
@@ -28,8 +28,7 @@ public class TripsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         mview = inflater.inflate(R.layout.fragment_trips, container, false);
         recyclerView = mview.findViewById(R.id.ongoingRV);
