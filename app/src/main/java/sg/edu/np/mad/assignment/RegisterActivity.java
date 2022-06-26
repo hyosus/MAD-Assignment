82f76463f5cbffe6037921b80f43d33898c75df9
hyosus <shaniceyeong@gmail.com>
2022-06-26 17:55:44 +0800

    Update manifest

--- app/src/main/java/sg/edu/np/mad/assignment/RegisterActivity.java
+++ app/src/main/java/sg/edu/np/mad/assignment/RegisterActivity.java
@@ -1,18 +1,17 @@
 package sg.edu.np.mad.assignment;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.util.Patterns;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -20,9 +19,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -168,12 +165,6 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
 
                     }
                 });
-
-
-
-
-
-
 
 
     }
